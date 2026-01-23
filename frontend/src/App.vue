c737e2de6116270e81f38c8f8309c7f156cc4a00
Logic Base Labs <logicbaselabs@gmail.com>
2026-01-23 23:03:14 +0100

    Fix invalid template structure by correcting conditional rendering blocks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,13 +11,22 @@ onMounted(() => {
   setInterval(store.updateSensors, 2000)
 })
 </script>
-
 <template>
   <div class="container py-4">
     <h2 class="text-center text-light mb-4">Sensor Dashboard</h2>
 
     <div class="row g-3">
-      <SensorCard v-for="sensor in store.sensors" :key="sensor.id" :sensor="sensor" />
+      <template v-if="store.sensors.length === 0">
+        <div class="col-12">
+          <p class="text-center text-light">
+            No sensor data available
+          </p>
+        </div>
+      </template>
+
+      <template v-else>
+        <SensorCard v-for="sensor in store.sensors" :key="sensor.id" :sensor="sensor" />
+      </template>
     </div>
   </div>
 </template>
