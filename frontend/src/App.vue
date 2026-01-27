8222565928ab579260ba62f942758bb0f0da1641
Logic Base Labs <logicbaselabs@gmail.com>
2026-01-27 10:17:02 +0100

    #12 Define MQTT topic structure for sensor data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
-import DashboardLayout from './components/dashboard/Layout.vue'
+import { onMounted } from 'vue'
+import { useSensorStore } from './stores/sensorStore'
+import { initDataService } from './services/dataService'
+
+const store = useSensorStore()
+
+onMounted(() => {
+  initDataService(store)
+})
 </script>
+
 
 <template>
   <DashboardLayout />
