1034cd9e73c0fce9ce51d03d3d4b00c0323292ae
Logic Base Labs <logicbaselabs@gmail.com>
2026-01-27 09:37:43 +0100

    Refactor App.vue to use modular dashboard layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,7 @@
 <script setup>
-import { onMounted } from 'vue'
-import { useSensorStore } from './stores/sensorStore'
-import SensorCard from './components/SensorCard.vue'
-
-const store = useSensorStore()
-
-onMounted(() => {
-  // MockDataService
-  store.updateSensors()
-  setInterval(store.updateSensors, 2000)
-})
+import DashboardLayout from './components/dashboard/DashboardLayout.vue'
 </script>
+
 <template>
-  <div class="container py-4">
-    <h2 class="text-center text-light mb-4">Sensor Dashboard</h2>
-
-    <div class="row g-3">
-      <template v-if="store.sensors.length === 0">
-        <div class="col-12">
-          <p class="text-center text-light">
-            No sensor data available
-          </p>
-        </div>
-      </template>
-
-      <template v-else>
-        <SensorCard v-for="sensor in store.sensors" :key="sensor.id" :sensor="sensor" />
-      </template>
-    </div>
-  </div>
+  <DashboardLayout />
 </template>
