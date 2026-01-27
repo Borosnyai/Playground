3381c5280b36d54e7952ffb32c85d2d1a53320f1
Logic Base Labs <logicbaselabs@gmail.com>
2026-01-27 19:11:13 +0100

    #22 #17 Add getters to group sensors by dashboard role

--- frontend/src/stores/sensorStore.js
+++ frontend/src/stores/sensorStore.js
@@ -12,5 +12,22 @@ export const useSensorStore = defineStore('sensor', {
       console.log('Sensors:', data)
       this.sensors = data
     }
+  },
+  getters: {
+    gaugeSensors: (state) =>
+      state.sensors.filter(
+        s => s.type === 'temperature' || s.type === 'air'
+      ),
+
+    monitoringSensors: (state) =>
+      state.sensors.filter(s => s.type === 'vibration'),
+
+    stateSensors: (state) =>
+      state.sensors.filter(s => s.type === 'light'),
+
+    valueSensors: (state) =>
+      state.sensors.filter(
+        s => s.type === 'distance' || s.type === 'pressure'
+      )
   }
 })
