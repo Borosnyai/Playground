ee8ef19e23cb8d39030e5d2bb94ad3b06e6156d1
Martin Orthacker <martinorthacker@gmail.com>
2026-03-28 13:47:31 +0100

    added another sensor

--- Python-Skripts/iodd_parser_basic.py
+++ Python-Skripts/iodd_parser_basic.py
@@ -10,10 +10,11 @@ load_dotenv()
 
 iodd_path = os.getenv("IODD_PATH")
 
-
+sensor1 = "Balluff-BOSR254KUUIPR10-ADSS-20210628-IODD1.1.zip"
+sensor2 = "Balluff-BOS21UUIRP30-20180207-IODD1.1.zip"
 
 # Parse and resolve in one step
-result = parser.parse_and_resolve(iodd_path + "Balluff-BOSR254KUUIPR10-ADSS-20210628-IODD1.1.zip")
+result = parser.parse_and_resolve(iodd_path + sensor2)
 
 print(result.device_name)
 print(result.device_manufacturer)
