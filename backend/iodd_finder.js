5fa7e3b436210bdbc94f40945afe3ffb7f36ca31
Martin Orthacker <martinorthacker@gmail.com>
2026-03-30 14:20:07 +0200

    getting basic data

--- backend/iodd_finder.js
+++ backend/iodd_finder.js
@@ -1,3 +1,23 @@
 import 'dotenv/config';
 
-console.log(process.env.IODD_URL)
+const API_url = process.env.IODD_URL
+
+const product_IDs = ["VVB302",
+"OGD551",
+"TA2115",
+"PL1514",
+"DV2310"]
+
+console.log(product_IDs);
+
+product_IDs.forEach(id => {
+    console.log(id);
+})
+const params = new URLSearchParams();
+params.append("productId", product_IDs[0]);
+console.log(API_url + "drivers?" + params);
+
+const data = await fetch(API_url + "drivers?" + params);
+const output = await data.json();
+
+console.log(output.content[0]);
