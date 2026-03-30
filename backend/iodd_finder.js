b0d5843b4928b184c8e23ea7fd7fc9c70ed79a21
Martin Orthacker <martinorthacker@gmail.com>
2026-03-30 15:25:53 +0200

    downloading zip from IODD-finder

--- backend/iodd_finder.js
+++ backend/iodd_finder.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
+import { writeFile } from 'node:fs/promises';
 
 const API_url = process.env.IODD_URL
+const api_key = process.env.IODD_FINDER_KEY
 
 const product_IDs = ["VVB302",
 "OGD551",
@@ -19,5 +21,21 @@ console.log(API_url + "drivers?" + params);
 
 const data = await fetch(API_url + "drivers?" + params);
 const output = await data.json();
+
+// getting iodd-zip:
+const vendorId = output.content[0].vendorId
+const ioddId = output.content[0].ioddId
+const zip = await fetch(API_url + `vendors/${vendorId}/iodds/${ioddId}/files/zip`, {headers:{
+    'X-API-KEY': api_key,
+    }
+});
+
+console.log(zip.headers)
+
+const filename = zip.headers.get('content-disposition')?.split('filename=')[1] || 'download.zip';
+
+await writeFile(filename, zip.body);
+
+console.log(API_url + `vendors/${vendorId}/iodds/${ioddId}/files/zip`);
 
 console.log(output.content[0]);
