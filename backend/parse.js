58adb5115cfc37b5ed6241cb31ab251b62e70669
Martin Orthacker <martinorthacker@gmail.com>
2026-03-31 09:47:41 +0200

    commenting on convert function

--- backend/parse.js
+++ backend/parse.js
@@ -43,10 +43,14 @@ export function get_hex(hexStr, startIndex, length, littleEndian = true){
 
 
 export function dec_to_hex(dec){
+    // convert to hex
     const hex = dec.toString(16);
+    // padding for full hex, e.g. 0x1 --> 0x0001
     const full_hex = hex.padStart(4,'0').toUpperCase();
+    // split hex into 2 values (first and second byte)
     const hex_01 = full_hex.substring(0,2);
     const hex_02 = full_hex.substring(2,4);
+    // convert to string:
     const result = hex_01 + '.' + hex_02;
     return result;
 }
