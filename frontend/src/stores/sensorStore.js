a5e18a6f3b04f0a3c543285f3d4a25acd7f03d4a
Logic Base Labs <logicbaselabs@gmail.com>
2026-04-02 13:19:26 +0200

    Resolve stash conflict and restore sensorStore

--- frontend/src/stores/sensorStore.js
+++ frontend/src/stores/sensorStore.js
@@ -1,17 +1,36 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useSensorStore = defineStore('sensor', {
   state: () => ({
     sensors: []
   }),
 
+  getters: {
+    gaugeSensors: (state) =>
+      state.sensors.filter(
+        s => s.type === 'temperature' || s.type === 'air'
+      ),
+
+    monitoringSensors: (state) =>
+      state.sensors.filter(s => s.type === 'vibration'),
+
+    stateSensors: (state) =>
+      state.sensors.filter(s => s.type === 'light'),
+
+    valueSensors: (state) =>
+      state.sensors.filter(
+        s => s.type === 'distance' || s.type === 'pressure'
+      )
+  },
+
   actions: {
-    updateSensors(sensors) {
+    setSensors(sensors) {
       this.sensors = sensors
     },
 
     updateSensor(sensor) {
       const index = this.sensors.findIndex(s => s.id === sensor.id)
+
       if (index !== -1) {
         this.sensors[index] = sensor
       } else {
@@ -19,5 +38,4 @@ export const useSensorStore = defineStore('sensor', {
       }
     }
   }
-
 })
