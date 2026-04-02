8bc0cca42c29591c16d2c68b01629caa2172106e
Logic Base Labs <logicbaselabs@gmail.com>
2026-04-02 18:46:29 +0200

    #36 recreting the last Stand

--- frontend/src/stores/sensorStore.js
+++ frontend/src/stores/sensorStore.js
@@ -5,27 +5,8 @@ export const useSensorStore = defineStore('sensor', {
     sensors: []
   }),
 
-  getters: {
-    gaugeSensors: (state) =>
-      state.sensors.filter(
-        s => s.type === 'temperature' || s.type === 'air'
-      ),
-
-    monitoringSensors: (state) =>
-      state.sensors.filter(s => s.type === 'vibration'),
-
-    stateSensors: (state) =>
-      state.sensors.filter(s => s.type === 'light'),
-
-    valueSensors: (state) =>
-      state.sensors.filter(
-        s => s.type === 'distance' || s.type === 'pressure'
-      )
-  },
-
   actions: {
-
-    setSensors(sensors) {
+    updateSensors(sensors) {
       this.sensors = sensors
     },
 
@@ -40,53 +21,3 @@ export const useSensorStore = defineStore('sensor', {
     }
   }
 })
-
-    updateSensors() {
-      const data = getSensors()
-      this.sensors = data.map(sensor => {
-        if (sensor.type === 'vibration') {
-          return {
-            ...sensor,
-            details: {
-              Velocity: `${sensor.velocity} mm/s`,
-              Frequency: `${sensor.frequency} Hz`,
-              Temperature: `${sensor.temperature} °C`
-            }
-          }
-        }
-        return sensor
-      })
-      updateSensors(sensors) {
-        this.sensors = sensors
-      },
-
-      updateSensor(sensor) {
-        const index = this.sensors.findIndex(s => s.id === sensor.id)
-        if (index !== -1) {
-          this.sensors[index] = sensor
-        } else {
-          this.sensors.push(sensor)
-        }
-
-      }
-    },
-
-    getters: {
-      gaugeSensors: (state) =>
-        state.sensors.filter(
-          s => s.type === 'temperature' || s.type === 'air'
-        ),
-
-      monitoringSensors: state =>
-        state.sensors.filter(s => s.type === 'vibration'),
-
-      stateSensors: state =>
-        state.sensors.filter(s => s.type === 'light'),
-
-      valueSensors: state =>
-        state.sensors.filter(
-          s => s.type === 'distance' || s.type === 'pressure'
-        )
-    }
-
-  })
