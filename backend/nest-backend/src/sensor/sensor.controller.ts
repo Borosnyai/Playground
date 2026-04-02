3b0931ba7977f83eb31582cd8c361e765939db01
Logic Base Labs <logicbaselabs@gmail.com>
2026-04-02 23:31:25 +0200

    #41step5: add sensor lookup by index

--- backend/nest-backend/src/sensor/sensor.controller.ts
+++ backend/nest-backend/src/sensor/sensor.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SensorService } from './sensor.service';
 
 @Controller('sensor')
 export class SensorController {
-  constructor(private readonly sensorService: SensorService ) {}
+  constructor(private readonly sensorService: SensorService) {}
 
   @Get()
   getSensorData() {
     return this.sensorService.getSensorData();
+  }
+  @Get(':index')
+  getSensorByIndex(@Param('index') index: string) {
+    return this.sensorService.getSensorByIndex(Number(index));
   } 
 }
+
 
