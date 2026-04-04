9fb7f8cc148c0d3df00acf2ce712e52fb7820eae
Logic Base Labs <logicbaselabs@gmail.com>
2026-04-04 22:00:20 +0200

    #1 Corrected the  Path for .env

--- python-service/iodd_api.py
+++ python-service/iodd_api.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 app = FastAPI()
 
-env_path = Path(__file__).resolve().parent / ".env"
+env_path = Path(__file__).resolve().parent.parent / ".env"
 
 load_dotenv(env_path)
 
