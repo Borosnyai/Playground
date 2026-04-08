cb26750f30362d407c7c29c93df6ee056ebf6bbf
Martin Orthacker <martinorthacker@gmail.com>
2026-04-08 23:50:14 +0200

    no more hardcodinggit diff ../package-lock.json!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 import SensorOverview from './components/SensorOverview.vue'
+import SensorsData from './components/SensorsData.vue'
 </script>
 
 <template>
-  <SensorOverview />
+  <SensorOverview/>
 </template>
